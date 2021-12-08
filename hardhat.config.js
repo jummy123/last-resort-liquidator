f794d7fa66a5000c5812349cceb1f215dcdb0d73
jummy123 <jummy123@users.noreply.github.com>
2021-12-08 09:50:37 +0000

    Fork from a specific block for tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,8 @@ module.exports = {
             gasPrice: 225000000000,
             initialBaseFeePerGas: 0,
             forking: {
-                url: 'https://api.avax.network/ext/bc/C/rpc'
+                url: 'https://api.avax.network/ext/bc/C/rpc',
+                blockNumber: 7300000
             },
             // brownie expects calls and transactions to throw on revert
             throwOnTransactionFailures: true,
